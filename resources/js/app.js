3d28de21f14ce875198bef65f175c81ba0f1d3af
Bedram Tamang <tmgbedu@gmail.com>
2020-08-07 17:14:41 +0545

    [Fix] Sidebar sroll problem fix. :label:

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ window._ = require("lodash");
 
 require('@/utils/flash.js');
 Vue.component('FileInput', () => import('@/components/FileInput'));
-Vue.component('editor', () => import('@/components/editor'));
+Vue.component('editor', () => import('@/components/Editor'));
 
 const app = new Vue({
     el: '#app',
